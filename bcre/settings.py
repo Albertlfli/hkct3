d843b7c868bbf84df6cfa1e98eec28153832ee87
Albertlfli <albertlfli@gmail.com>
2024-09-05 13:47:40 +0800

    abc

--- bcre/settings.py
+++ bcre/settings.py
@@ -78,8 +78,11 @@ WSGI_APPLICATION = 'bcre.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'bore',
+        'USER': 'albertli',
+        'PASSWORD': '1288',
+        "HOST": 'localhost',
     }
 }
 
@@ -128,3 +131,6 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
--- listings/views.py
+++ listings/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+listings = Listing.objects.all()
+context = {'listings':listings}
+return render( request, 'listings/listings.html', context)
+
+
+def index(request):
+    return render(request,'listings/listings.html', {'name' : 'something'})
+
+def listing(request):
+    return render(request,'listings/listing.html')
+
+def search(request):
+    return render(request,'listings/search.html')
